7d1cdeab4c829e9193457af4ee2b1e0e929c3d21
amen ramadan <amen@MacbookShops-MacBook-Pro.local>
2024-06-01 20:48:09 +0300

    v1.2.2

--- app/productDetails/[productId]/_components/ProductBanner.jsx
+++ app/productDetails/[productId]/_components/ProductBanner.jsx
@@ -4,11 +4,15 @@ import Image  from 'next/image';
 export default function ProductBanner({ product }) {
   return (
     <div>
-      <Image
-        alt='product-details-banner'
-        width={ 400 } height={ 400 }
-        src={ product?.attributes?.banner?.data?.attributes?.url }
-        className='rounded-lg' />
+      { product?.attributes?.banner?.data?.attributes?.url ?
+        <Image
+          alt='product-details-banner'
+          width={ 400 } height={ 400 }
+          src={ product?.attributes?.banner?.data?.attributes?.url }
+          className='rounded-lg' />
+        :
+        <div className='w-[400px] h-[225px] bg-slate-200 rounded-lg animate-pulse'></div>
+      }
     </div>
   )
 }
--- app/productDetails/[productId]/_components/ProductInfo.jsx
+++ app/productDetails/[productId]/_components/ProductInfo.jsx
@@ -1,16 +1,42 @@
-import React from 'react'
-import  {AlertOctagon, BadgeCheck, ShoppingCart} from 'lucide-react';
+import React from "react";
+import { AlertOctagon, BadgeCheck, ShoppingCart } from "lucide-react";
+import SkeletonProductInfo from "./SkeletonProductInfo";
 
-export default function ProductInfo({product}) {
+export default function ProductInfo({ product }) {
   return (
     <div>
-      <h2 className='text-[20px]'>{ product?.attributes?.title }</h2>
-      <p className='text-[16px] text-gray-400'>{ product?.attributes?.category }</p>
-      <p className='text-[16px] mt-5'>{ product?.attributes?.description[ 0 ]?.children[ 0 ].text }</p>
-      <p className='text-[11px] text-gray-500 flex gap-2 mt-2 items-center'> { product?.attributes?.instantDelivery ? <BadgeCheck className='w-4 h-4 text-gray-500'/> : <AlertOctagon/>}Eligible For Instant Delivery</p>
-      <p className='text-[32px] mt-3 text-primary'> ${ product?.attributes?.price }</p>
+      { product?.attributes?.banner?.data?.attributes?.url ? (
+        <div>
+        <h2 className="text-[20px]">{product?.attributes?.title}</h2>
+        <p className="text-[16px] text-gray-400">
+          {product?.attributes?.category}
+        </p>
+        <p className="text-[16px] mt-5">
+          {product?.attributes?.description[0]?.children[0].text}
+        </p>
+        <p className="text-[11px] text-gray-500 flex gap-2 mt-2 items-center">
+          {" "}
+          {product?.attributes?.instantDelivery ? (
+            <BadgeCheck className="w-4 h-4 text-gray-500" />
+          ) : (
+            <AlertOctagon />
+          )}
+          Eligible For Instant Delivery
+        </p>
+        <p className="text-[32px] mt-3 text-primary">
+          {" "}
+          ${product?.attributes?.price}
+        </p>
 
-      <button className='text-white rounded-md flex gap-2 hover:bg-teal-600 p-3  bg-primary'> <ShoppingCart/>Add to cart</button>
+        <button className="text-white rounded-md flex gap-2 hover:bg-teal-600 p-3  bg-primary">
+          {" "}
+          <ShoppingCart />
+          Add to cart
+        </button>
+        </div>
+      ) : (
+        <SkeletonProductInfo />
+      )}
     </div>
-  )
+  );
 }
